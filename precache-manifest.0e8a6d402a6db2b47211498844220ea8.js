bf8fcc7d710b843992d388e165a4adea3b577b63
Marius Gava <49473703+0XLR8@users.noreply.github.com>
2020-06-24 23:18:53 +0300

    Add files via upload

---
+++ precache-manifest.0e8a6d402a6db2b47211498844220ea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aaa5022ba51360c2a97036e7375a2a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7525bf55304a81d62bfb",
+    "url": "/static/css/main.bfbcb60f.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7525bf55304a81d62bfb",
+    "url": "/static/js/main.ac618f4e.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
